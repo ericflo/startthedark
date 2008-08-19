917905ca63ef5f0cd01ce00a1cffb827dd834f6c
Eric Florenzano <ericflo@e.local>
2008-08-18 23:50:46 -0500

    Initial commit

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^startthedark/', include('startthedark.foo.urls')),
+
+    # Uncomment the next line to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line for to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+)
